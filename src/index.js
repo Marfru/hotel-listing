f9cf01b0deec59619dd004fee2bd9cb395d7af61
Marcos Frutos <marcos.frutos@helloprint.com>
2020-05-27 11:09:45 +0200

    uploading project:

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "babel-polyfill";
+import { render } from 'melody-streams';
+import './scss/index.scss';
+import home from './main';
+
+const documentRoot = document.getElementById('root');
+render(documentRoot, home);
